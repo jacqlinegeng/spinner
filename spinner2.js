39a676c132ddca43ea2824e4e7712ac487e543cd
Jacqline <jacqlinegeng@gmail.com>
2022-08-30 16:58:24 -0400

    refactor spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,25 +1,3 @@
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 100);
-
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   ');
-// }, 700);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r|   ');
-// }, 900);
-
 let animation = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ','\n'];
 
 let time = 0;
